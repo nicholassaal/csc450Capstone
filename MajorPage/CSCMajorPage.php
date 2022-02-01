edf27c67e84b70e1835d9b89094f71927f6d2158
Mitchell Williamson <williamm15@csp.edu>
2022-02-01 16:22:55 -0600

    Created major pages

---
+++ MajorPage/CSCMajorPage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Majors</title>
+    <link rel="stylesheet" type="text/css" href="majorStyle.css">
+</head>
+
+<body>
+    <header>
+        <nav class="topNav">
+            <ul>
+                <li class="logo"><a href=" ">Home</a></li>
+                <li><a href=" ">Home</a></li>
+                <li><a href=" ">My Profile</a></li>
+                <li><a href=" ">Majors</a></li>
+                <li><a href="LoginPage.php">Sign Out</a></li>
+            </ul>
+        </nav>
+        <h1>Computer Science Courses</h1>
+    </header>
+    <section class="course-container">
+        <div>CLASS NAME HERE<div>CLASS DESCRIPTION GOES HERE</div></div>
+        <div>CLASS NAME HERE<div>CLASS DESCRIPTION GOES HERE</div></div>
+        <div>CLASS NAME HERE<div>CLASS DESCRIPTION GOES HERE</div></div>
+        <div>CLASS NAME HERE<div>CLASS DESCRIPTION GOES HERE</div></div>
+        <div>CLASS NAME HERE<div>CLASS DESCRIPTION GOES HERE</div></div>
+    </section>
+    <section class="course-container">
+        <div>CLASS NAME HERE<div>CLASS DESCRIPTION GOES HERE</div></div>
+        <div>CLASS NAME HERE<div>CLASS DESCRIPTION GOES HERE</div></div>
+        <div>CLASS NAME HERE<div>CLASS DESCRIPTION GOES HERE</div></div>
+        <div>CLASS NAME HERE<div>CLASS DESCRIPTION GOES HERE</div></div>
+        <div>CLASS NAME HERE<div>CLASS DESCRIPTION GOES HERE</div></div>
+    </section>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
